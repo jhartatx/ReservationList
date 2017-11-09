efed4b6823897a2b94ee6e724f3f7fbe610ac740
jhartatx <jhartatx@gmail.com>
2017-11-09 11:57:05 -0600

    Final upload

---
+++ reservation.js
@@ -0,0 +1,32 @@
+// var Reservations = function(name, number, email, id){
+// this.name = name;
+// this.number = number;
+// this.email = email;
+// this.id = id;
+//
+// };
+
+
+var reservationList =  [
+{
+"name": "James Andrews",
+"number": "5129098989",
+"email": "jandrews@aol.com",
+"id": "andrews"
+},
+{
+"name": "John Smith",
+"number": "5129768989",
+"email": "jsmith@aol.com",
+"id": "jj101"
+},
+{
+"name": "Justin Cole",
+"number": "9792556767",
+"email": "jcole@aol.com",
+"id": "cole"
+}
+
+]
+
+module.exports = reservationList;
